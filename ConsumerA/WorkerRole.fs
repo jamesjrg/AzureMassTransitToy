a4b8980161e3b68b342d4fa68cba2ab5ebf020d5
James Gregory <james.jrg@gmail.com>
2015-04-13 12:57:56 +0100

    A couple more stub projects

---
+++ ConsumerA/WorkerRole.fs
@@ -0,0 +1,33 @@
+﻿namespace ConsumerA
+
+open System
+open System.Collections.Generic
+open System.Diagnostics
+open System.Linq
+open System.Net
+open System.Threading
+open Microsoft.WindowsAzure
+open Microsoft.WindowsAzure.Diagnostics
+open Microsoft.WindowsAzure.ServiceRuntime
+
+type WorkerRole() =
+    inherit RoleEntryPoint()
+
+    let log message (kind : string) = Trace.TraceInformation(message, kind)
+
+    override wr.Run() =
+
+        log "ConsumerA entry point called" "Information"
+        while(true) do 
+            Thread.Sleep(10000)
+            log "Working" "Information"
+
+    override wr.OnStart() = 
+
+        // Set the maximum number of concurrent connections 
+        ServicePointManager.DefaultConnectionLimit <- 12
+       
+        // For information on handling configuration changes
+        // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+
+        base.OnStart()
